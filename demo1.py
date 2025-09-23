a5d888765d30196331831d95dde3fe39473d1c2d
Paul Gribble <pgribble@uwo.ca>
2025-09-23 07:51:31 -0400

    misc

--- demo1.py
+++ demo1.py
@@ -58,7 +58,7 @@ optimizer = th.optim.Adam(policy.parameters(), lr=1e-3)
 # Training loop
 
 n_batch       =  3000
-batch_size    =   256
+batch_size    =    32
 task.run_mode = 'train' # random reaching across the workspace
 FF_k          = 0.0     # force-field strength
 
