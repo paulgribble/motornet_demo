a68b784bc71c5d0b18b325695c1fe1fde5193e15
Paul Gribble <pgribble@uwo.ca>
2025-09-24 13:56:43 -0400

    loss hidden up

--- my_loss.py
+++ my_loss.py
@@ -7,7 +7,7 @@ loss_weights = {
     'jerk'              : 1e+4, #1e+2
     'muscle'            : 1e-1, #1e-2
     'muscle_derivative' : 1e-2,
-    'hidden'            : 1e-1, #1e-0
+    'hidden'            : 1e-0,
     'hidden_derivative' : 1e+1
     }
 
