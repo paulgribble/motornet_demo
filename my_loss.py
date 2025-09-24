626a26bd581e2bb1507056eeadcdd19cef0a14e8
Paul Gribble <pgribble@uwo.ca>
2025-09-24 13:29:35 -0400

    speed up loss calculations

--- my_loss.py
+++ my_loss.py
@@ -12,7 +12,7 @@ loss_weights = {
     }
 
 
-def calculate_loss(episode_data):
+def calculate_loss_orig(episode_data):
 
     losses = {}
 
@@ -32,5 +32,28 @@ def calculate_loss(episode_data):
     losses['total'] = sum(losses[k] for k in losses.keys())
 
     return losses
+
+
+def calculate_loss(episode_data):
+    # Pre-compute common operations
+    speed_data  = episode_data['xy'][:,:,2:]
+    jerk_data   = th.diff(speed_data,             n=2, dim=1)
+    force_diff  = th.diff(episode_data['force'],  n=1, dim=1)
+    hidden_diff = th.diff(episode_data['hidden'], n=2, dim=1)
+    
+    # Calculate losses with gradients enabled
+    losses = {
+        'position'         : loss_weights['position']          * th.mean(th.abs(episode_data['xy'][:,:,0:2] - episode_data['targets'])),
+        'speed'            : loss_weights['speed']             * th.mean(th.square(speed_data)),
+        'jerk'             : loss_weights['jerk']              * th.mean(th.square(jerk_data)),
+        'muscle'           : loss_weights['muscle']            * th.mean(episode_data['force']),
+        'muscle_derivative': loss_weights['muscle_derivative'] * th.mean(th.square(force_diff)),
+        'hidden'           : loss_weights['hidden']            * th.mean(th.square(episode_data['hidden'])),
+        'hidden_derivative': loss_weights['hidden_derivative'] * th.mean(th.square(hidden_diff))
+    }
+    
+    losses['total'] = sum(losses.values())
+    return losses
+
 
 
