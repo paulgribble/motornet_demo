9d006439df05afc1df0982440dedbb64fcef4bb6
Paul Gribble <pgribble@uwo.ca>
2025-09-28 21:43:20 -0400

    n_batch 10000

--- demo1.py
+++ demo1.py
@@ -66,7 +66,7 @@ optimizer = th.optim.Adam(policy.parameters(), lr=1e-3)
 
 # Main training loop
 
-n_batch       =  5000
+n_batch       = 10000
 interval      =   100   # for intermediate plots
 batch_size    =    32
 FF_k          =     0   # force-field strength
