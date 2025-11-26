1f90b42fcdb4d7fe375dc37b7145deefd4a427ad
Paul Gribble <pgribble@uwo.ca>
2025-11-25 19:32:03 -0500

    1st crack at modular

---
+++ demo1_modular.py
@@ -0,0 +1,184 @@
+import os
+os.environ['OMP_NUM_THREADS'] = '1'
+os.environ['MKL_NUM_THREADS'] = '1' # Set this too just in case
+os.environ["VECLIB_MAXIMUM_THREADS"] = '1'
+
+import torch as th
+th._dynamo.reset()                          # clear compilation cache
+th.set_num_threads(1)                       # intra-op
+th.set_num_interop_threads(1)               # inter-op
+th._dynamo.config.cache_size_limit = 64     # Smaller cache for CPU workloads
+
+import numpy as np
+import matplotlib.pyplot as plt
+import motornet_plain as mn
+from tqdm import tqdm
+import pickle
+
+from mehrdad_policies import ModularPolicyGRU         # the modular RNN
+from my_loss   import calculate_loss # the loss function
+from my_env    import ExperimentEnv  # the environment
+from my_task   import ExperimentTask # the task
+from my_utils  import run_episode    # run a batch of simulations
+from my_utils  import plot_losses    # for plotting loss history
+from my_utils  import plot_handpaths # for plotting hand paths
+from my_utils  import plot_signals_modular   # for plotting inputs and outputs per trial
+from my_utils  import save_model_modular     # for saving model config, weights, losses to disk
+from my_utils  import load_model_modular     # for loading model config and weights from disk
+
+print('All packages imported.')
+print('pytorch version: ' + th.__version__)
+print('numpy version: ' + np.__version__)
+print('motornet version: ' + mn.__version__)
+
+
+########## DEFINE A NEW MODEL AND TRAIN IT ON RANDOM REACHES ##########
+
+# use the cpu not the gpu
+device = th.device("cpu")
+
+# define a two-joint planar arm using a Hill-type muscle model as described in:
+#   Kistemaker, Wong & Gribble (2010) J. Neurophysiol. 104(6):2985-94
+effector = mn.effector.RigidTendonArm26(muscle=mn.muscle.RigidTendonHillMuscle())
+
+# initialize the experimental environment
+ep_dur = 3.00 # duration of each simulation (seconds)
+env = ExperimentEnv(effector=effector, max_ep_duration=ep_dur,
+                    proprioception_delay=0.02, vision_delay=0.07,
+                    proprioception_noise=1e-3, vision_noise=1e-3, action_noise=1e-4
+                    )
+
+obs, info = env.reset()
+n_t = int(ep_dur / env.effector.dt)
+
+# define the experimental task
+task = ExperimentTask(effector=env.effector)
+inputs, targets, init_states, _ = task.generate(1, n_t) # get sample inputs so we know how many for policy
+n_task_inputs = inputs['inputs'].shape[2]
+task_dim = np.arange(inputs['inputs'].shape[-1])
+vision_dim = np.arange(env.get_vision().shape[1]) + task_dim[-1] + 1
+proprio_dim = np.arange(env.get_proprioception().shape[1]) + vision_dim[-1] + 1
+
+# define the modular RNN
+policy = ModularPolicyGRU(input_size  = env.observation_space.shape[0] + n_task_inputs,
+                          module_size = [256, 256, 32],
+                          output_size = env.n_muscles,
+                          vision_dim  = vision_dim,
+                          proprio_dim = proprio_dim,
+                          task_dim    = task_dim,
+                          vision_mask = [0.2, 0.0, 0.0],
+                          proprio_mask = [0.0, 0.0, 0.5],
+                          task_mask = [0.2, 0.02, 0.0],
+                          connectivity_mask = np.array([[1. , 0.2 , 0.02], [0.2 , 1.  , 0.2 ], [0.  , 0.2 , 1.  ]]),
+                          output_mask = [0.0, 0.0, 0.5],
+                          connectivity_delay = np.zeros((3,3)),
+                          spectral_scaling = 1.1,
+                          device = device,
+                          activation = 'tanh')
+
+# Set the optimizer and constraints
+optimizer = th.optim.Adam(policy.parameters(), lr=1e-3)
+
+
+# --------------------------------------------------
+# TRAINING 
+# --------------------------------------------------
+
+n_batch       = 10000
+interval      =   100   # for intermediate plots
+batch_size    =    64
+FF_k          =     0   # force-field strength
+save_name     = "demo1_modular" # name to use to save model, plots, etc
+
+# Create a directory to store output plots and files if it doesn't already exist
+if not os.path.exists(save_name):
+        print(f"creating directory {save_name}/")
+        os.mkdir(save_name)
+
+# Pre-allocate numpy arrays instead of lists for better memory efficiency
+loss_history = {
+    "total"             : np.zeros(n_batch, dtype=np.float32),
+    "position"          : np.zeros(n_batch, dtype=np.float32),
+    "speed"             : np.zeros(n_batch, dtype=np.float32),
+    "jerk"              : np.zeros(n_batch, dtype=np.float32),
+    "muscle"            : np.zeros(n_batch, dtype=np.float32),
+    "muscle_derivative" : np.zeros(n_batch, dtype=np.float32),
+    "hidden"            : np.zeros(n_batch, dtype=np.float32),
+    "hidden_derivative" : np.zeros(n_batch, dtype=np.float32),
+}
+
+# Optimize loss keys for faster iteration
+loss_keys = ["total", "position", "speed", "jerk", 
+             "muscle", "muscle_derivative", "hidden", "hidden_derivative"]
+
+# main training loop
+print(f"main training loop")
+print(f"simulating {ep_dur} second movements using {n_t} time points")
+
+for i in tqdm(
+    iterable = range(n_batch), 
+    desc     = f"training {n_batch} batches of {batch_size}", 
+    unit     = "batch", 
+    total    = n_batch, 
+):
+    
+    task.run_mode = 'train'                                      # random reaches in workspace
+    episode_data = run_episode(env, task, policy, batch_size, n_t, device, k=FF_k) # run the batch forwards
+    loss = calculate_loss(episode_data)                          # compute loss
+    loss['total'].backward()                                     # propagate loss backwards to compute gradients
+    th.nn.utils.clip_grad_norm_(policy.parameters(), max_norm=1) # so that gradients don't get out of hand
+    optimizer.step()                                             # adjust network weights
+    optimizer.zero_grad(set_to_none=True)                        # zero out the gradients
+    
+    # Track losses
+    with th.no_grad():  # Don't need gradients for loss tracking
+        for key in loss_keys:
+            loss_history[key][i] = loss[key].item()
+    
+    # plot some things every once in a while
+    if (i>0) and (i % interval == 0):
+        # Convert to lists the portions we need for plotting
+        current_history = {key: loss_history[key][:i+1].tolist() for key in loss_keys}
+        plot_losses(loss_history=current_history, fname=os.path.join(save_name, f"{save_name}_losses.png"))
+        plot_handpaths(episode_data=episode_data, fname=os.path.join(save_name, f"{save_name}_handpaths.png"), figtitle=f"batch {i:04d} (n={batch_size})")
+        for j in range(4):  # plot first 4 trials of batch
+            plot_signals_modular(episode_data=episode_data, fname=os.path.join(save_name,f"{save_name}_signals_{j}.png"), figtitle=f"batch {i:04d} (n={batch_size})", trial=j)
+
+# Convert back to lists
+for key in loss_keys:
+    loss_history[key] = loss_history[key].tolist()
+
+# save the model to disk
+save_model_modular(env, policy, loss_history, save_name)
+
+
+# --------------------------------------------------
+# TESTING
+# --------------------------------------------------
+
+# LOAD A MODEL AND TEST IT ON CENTER-OUT REACHES
+
+save_name = "demo1_modular"
+
+n_tg     = 8    # number of targets for center-out task
+sim_time = 3.00 # simulation time (seconds)
+FF_k     = 0    # FF strength
+
+print(f"loading model {save_name}")
+env,task,policy,device = load_model_modular(os.path.join(save_name,f"{save_name}_cfg.json"), os.path.join(save_name,f"{save_name}_weights.pkl"))
+
+n_t = int(sim_time / env.dt)           # number of simulation steps
+task.run_mode = 'test_center_out'      # center-out reaches
+
+print(f"simulating {task.run_mode}")
+episode_data = run_episode(env, task, policy, n_tg, n_t, device, k=FF_k) # run the batch forwards
+
+# Plot results
+plot_handpaths(episode_data=episode_data, fname=os.path.join(save_name,f"{save_name}_handpaths_test.png"))
+for i in range(n_tg):
+    plot_signals_modular(episode_data=episode_data, fname=os.path.join(save_name,f"{save_name}_signals_test_{i}.png"), figtitle=f"trial {i}", trial=i)
+
+# save episide data to a .pkl file on disk
+with open(os.path.join(save_name,f"{save_name}_episode_data.pkl"), "wb") as f:
+    pickle.dump(episode_data, f)
+
--- my_utils.py
+++ my_utils.py
@@ -8,6 +8,7 @@ import motornet as mn
 from my_env    import ExperimentEnv  # the environment
 from my_task   import ExperimentTask # a task
 from my_policy import Policy         # the RNN
+from mehrdad_policies import ModularPolicyGRU         # the modular RNN
 
 # Apply a curl force field
 def applied_load(endpoint_vel, k, mode = 'CW'):
@@ -189,6 +190,73 @@ def plot_signals(episode_data, fname="", figtitle="", trial=0, coord="xy"):
         plt.show()
         return fig,ax
 
+def plot_signals_modular(episode_data, fname="", figtitle="", trial=0, coord="xy"):
+    if (coord=="xy"):
+        xy = episode_data['xy'].detach()[:,:,0:2]
+        vel = episode_data['xy'].detach()[:,:,2:]
+        tg = episode_data['targets'].detach()
+        inp = episode_data['inp'].detach()
+    elif (coord=="joint"):
+        xy = episode_data['joint'].detach()[:,:,0:2] * 180/np.pi
+        vel = episode_data['joint'].detach()[:,:,2:] * 180/np.pi
+        tg = xy_to_joints(episode_data['targets'][:,:,0:2].detach(), episode_data['l1'], episode_data['l2']) * 180 / np.pi
+        inp = episode_data['inp'].detach().numpy()
+        inp_tmp = inp.copy()
+        inp[:,:,0:2] = xy_to_joints(inp_tmp[:,:,0:2], episode_data['l1'], episode_data['l2']) * 180 / np.pi
+    hidden = episode_data['hidden'].detach()
+    activation = episode_data['muscle'].detach()
+    fig = plt.figure(figsize=(6, 13), constrained_layout=True)
+    gs = gridspec.GridSpec(6, 1, figure=fig, height_ratios=[1, 2, 2, 2, 4, 4])
+    ax0 = fig.add_subplot(gs[0])
+    ax1 = fig.add_subplot(gs[1])
+    ax2 = fig.add_subplot(gs[2])
+    ax3 = fig.add_subplot(gs[3])
+    ax4 = fig.add_subplot(gs[4])
+    ax5 = fig.add_subplot(gs[5])
+    ax = [ax0, ax1, ax2, ax3, ax4, ax5]
+    ax[0].plot(inp[trial,:,2],'-')
+    ax[0].set_ylabel('GO CUE [0,1]')
+    ax[0].set_ylim([-0.01,1.01])
+    ax[1].plot(inp[trial,:,0],':')
+    ax[1].plot(xy[trial,:,0],'-')
+    ax[1].plot(tg[trial,:,0],'--')
+    if (coord=="xy"):
+        ax[1].set_ylabel('X (m)')
+    elif (coord=="joint"):
+        ax[1].set_ylabel('SHOULDER (deg)')
+    ax[2].plot(inp[trial,:,1],':')
+    ax[2].plot(xy[trial,:,1],'-')
+    ax[2].plot(tg[trial,:,1],'--')
+    if (coord=="xy"):
+        ax[2].set_ylabel('Y (m)')
+    elif (coord=="joint"):
+        ax[2].set_ylabel('ELBOW (deg)')
+    ax[3].plot(vel[trial,:,:],'-')
+    if (coord=="xy"):
+        ax[3].set_ylabel('XY VEL (m/s)')
+    elif (coord=="joint"):
+        ax[3].set_ylabel('JOINT VEL (deg/s)')
+    ax[4].plot(hidden[trial,:,:],'-', alpha=0.25)
+    ax[4].set_ylabel('GRU HIDDEN')
+    ax[5].plot(activation[trial,:,:],'-')
+    ax[5].set_ylabel('MUSCLE ACTIVATION [0,1]')
+    ax[5].set_xlabel('TIME (steps)')
+    for i in range(6):
+        ax[i].spines['top'].set_visible(False)
+        ax[i].spines['right'].set_visible(False)
+        ax[i].set_xlim([0,np.shape(xy)[1]])
+        ax[i].set_xticks(np.arange(0, np.shape(xy)[1], 50))
+        if i<4:
+            ax[i].set_xticks([])
+            ax[i].tick_params(axis='x', length=0)
+            ax[i].spines['bottom'].set_visible(False)
+    fig.suptitle(figtitle, fontsize=14)
+    if not fname=="":
+        fig.savefig(fname)
+        plt.close(fig)
+    else:
+        plt.show()
+        return fig,ax
 
 def xy_to_joints_helper(xy, l1, l2):
     a0,a1 = 0,0
@@ -241,6 +309,29 @@ def save_model(env, policy, losses, model_name, quiet=False):
         print(f"saved {losses_file}")
         print(f"saved {cfg_file}")
 
+def save_model_modular(env, policy, losses, model_name, quiet=False):
+    weight_file = os.path.join(model_name, model_name + "_weights.pkl")
+    losses_file = os.path.join(model_name, model_name + "_losses.json")
+    cfg_file    = os.path.join(model_name, model_name + "_cfg.json")
+
+    # save model weights
+    th.save(policy.state_dict(), weight_file)
+
+    # save training history (log)
+    with open(losses_file, 'w') as file:
+        json.dump(losses, file)
+
+    # save environment configuration dictionary
+    cfg = env.get_save_config()
+    cfg['module_size'] = policy.module_size
+    with open(cfg_file, 'w') as file:
+        json.dump(cfg, file)
+
+    if (quiet == False):
+        print(f"saved {weight_file}")
+        print(f"saved {losses_file}")
+        print(f"saved {cfg_file}")
+
 
 def load_model(cfg_file, weight_file):
     # load a previously trained model
@@ -266,5 +357,51 @@ def load_model(cfg_file, weight_file):
     task = ExperimentTask(effector=env.effector)
     inputs, _, _, _ = task.generate(1, 1) # just to get input shape
     policy = Policy(env.observation_space.shape[0] + inputs['inputs'].shape[2], n_hidden, env.n_muscles, device=device)
+    policy.load_state_dict(w)
+    return env, task, policy, device
+
+def load_model_modular(cfg_file, weight_file):
+    # load a previously trained modular model
+    device = th.device("cpu")
+    cfg = json.load(open(cfg_file, 'r'))
+    dt = cfg['effector']['dt']
+    muscle_name = cfg['effector']['muscle']['name']
+    muscle = getattr(mn.muscle, muscle_name)()
+    effector = mn.effector.RigidTendonArm26(muscle=muscle, timestep=dt)
+    proprioception_delay = cfg['proprioception_delay']*cfg['dt']
+    vision_delay = cfg['vision_delay']*cfg['dt']
+    action_noise = cfg['action_noise'][0]
+    proprioception_noise = cfg['proprioception_noise'][0]
+    vision_noise = cfg['vision_noise'][0]
+    max_ep_duration = cfg['max_ep_duration']
+    env = ExperimentEnv(effector=effector, max_ep_duration=max_ep_duration,
+                        proprioception_delay=proprioception_delay, vision_delay=vision_delay,
+                        proprioception_noise=proprioception_noise, vision_noise=vision_noise,
+                        action_noise=action_noise
+                        )
+    w = th.load(weight_file, weights_only=True)
+    task = ExperimentTask(effector=env.effector)
+    inputs, _, _, _ = task.generate(1, 1) # just to get input shape
+    n_task_inputs = inputs['inputs'].shape[2]
+    task_dim = np.arange(inputs['inputs'].shape[-1])
+    vision_dim = np.arange(env.get_vision().shape[1]) + task_dim[-1] + 1
+    proprio_dim = np.arange(env.get_proprioception().shape[1]) + vision_dim[-1] + 1
+
+    # define the modular RNN
+    policy = ModularPolicyGRU(input_size  = env.observation_space.shape[0] + n_task_inputs,
+                            module_size = cfg['module_size'],
+                            output_size = env.n_muscles,
+                            vision_dim  = vision_dim,
+                            proprio_dim = proprio_dim,
+                            task_dim    = task_dim,
+                            vision_mask = [0.2, 0.0, 0.0],
+                            proprio_mask = [0.0, 0.0, 0.5],
+                            task_mask = [0.2, 0.02, 0.0],
+                            connectivity_mask = np.array([[1. , 0.2 , 0.02], [0.2 , 1.  , 0.2 ], [0.  , 0.2 , 1.  ]]),
+                            output_mask = [0.0, 0.0, 0.5],
+                            connectivity_delay = np.zeros((3,3)),
+                            spectral_scaling = 1.1,
+                            device = device,
+                            activation = 'tanh')
     policy.load_state_dict(w)
     return env, task, policy, device
