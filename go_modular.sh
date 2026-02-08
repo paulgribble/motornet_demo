3755621cbcf70cc74a9dd9953375c192bd167f65
Paul Gribble <pgribble@uwo.ca>
2026-02-08 14:03:39 -0500

    cmd line name

--- go_modular.sh
+++ go_modular.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-uv run python reaching_model.py create demo1_modular --modular
-uv run python reaching_model.py train demo1_modular --batches 20000
-uv run python reaching_model.py test demo1_modular
-uv run jupyter nbconvert --to notebook --execute --inplace --ExecutePreprocessor.timeout=-1 demo1_modular_visualize.ipynb
+uv run python reaching_model.py create "$1" --modular
+uv run python reaching_model.py train "$1" --batches 20000
+uv run python reaching_model.py test "$1"
