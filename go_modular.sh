ce82c092945ab12838735e3f1f4351db1ef48cb7
Paul Gribble <pgribble@uwo.ca>
2026-02-08 14:04:03 -0500

    cmd line name check

--- go_modular.sh
+++ go_modular.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+if [ -z "$1" ]; then
+  echo "Usage: $0 <name>"
+  exit 1
+fi
+
 uv run python reaching_model.py create "$1" --modular
 uv run python reaching_model.py train "$1" --batches 20000
 uv run python reaching_model.py test "$1"
