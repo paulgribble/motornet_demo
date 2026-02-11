fefb60ba57eab3243deb01c321a6adb0d70ec907
Paul Gribble <pgribble@uwo.ca>
2026-02-11 11:27:47 -0500

    hidden loss increase

--- go_modular.sh
+++ go_modular.sh
@@ -6,6 +6,6 @@ if [ -z "$1" ]; then
 fi
 
 uv run python reaching_model.py create "$1"
-uv run python reaching_model.py train "$1" --batches 20000
+uv run python reaching_model.py train "$1" --batches 20000 --batch-size 32
 uv run python reaching_model.py train "$1" --batches 500 --task center_out
 uv run python reaching_model.py test "$1"
